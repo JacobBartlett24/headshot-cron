6c780f864cb5d0ebcc1d47a4e257c2f281802c3e
Jacob Bartlett <96956475+JacobBartlett24@users.noreply.github.com>
2024-06-18 21:10:23 +0000

    Add loggin

--- index.js
+++ index.js
@@ -1,33 +1,45 @@
+console.log("Starting...")
+
 import pg from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
 const { Pool } = pg;
 
+console.log("Creating Pool...")
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
 })
+console.log("Done Creating Pool...")
 
+console.log("Connecting To Pool...")
 pool.connect(function(err, client, done) {
     if (err) {
       console.log(`Not able to get a connection: ${err}.`);
       response.status(400).send(err);
     }
+    console.log("Start Fetching Matches...")
     fetchMatches(client, done)
-    
+    console.log("Finish Fetching Matches...")
     pool.end();
 });
+console.log("Done Connecting To Pool...")
+
+
 
 async function fetchMatches(client, done) {
+    console.log("Fetching From HLTV...")
     const response = await fetch("https://hltv.org", {
       method: 'GET',
       headers: {
       },
     });
-  
+    console.log("Done Fetching From HLTV...")
+
     let html = await response.text();
     if (response.status === 200) {
       try {
+        console.log("Start Database Ops...")
         // Clear the existing HTML entries
         await client.query('DELETE FROM html');
         // Insert new HTML content using parameterized query
@@ -37,9 +49,10 @@ async function fetchMatches(client, done) {
         const result = await client.query('SELECT * FROM html');
         // Perform cleanup or release resources if necessary
         done();
-        console.log(`Success Date ${new Date()}`)
+        console.log("Finish Database Ops...")
       } catch (error) {
         console.error('Error processing HTML content:', error);
       }
     }
   }
+  console.log(`Success Date ${new Date()}`)
